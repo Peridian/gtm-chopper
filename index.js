b75131c42cfa61f18009f4e7c80913e1d0b6ef54
peridian <arthurborbalopes@gmail.com>
2016-10-23 00:49:52 -0200

    most thigs done. now trying to create container with excluded tags

---
+++ index.js
@@ -0,0 +1,72 @@
+'use strict'
+
+console.log()
+
+var
+  indexPath = ''
+  , splitLevel = ''
+  , searchKey = ''
+  , rootPath = ''
+  , notWrite = ''
+  , logFormat = ''
+  , excludeTags = ''
+  ;
+
+notWrite = process.argv.indexOf('notWrite') != -1 ? false : true
+
+process.argv.forEach(function (val, index, array) {
+
+  indexPath = array[1].split('\\')
+  indexPath.pop()
+  indexPath.join('\\')
+
+  if (val.indexOf('folder') != -1) indexPath = val.split('folder=').pop()
+  else if (val.indexOf('key') != -1) searchKey = val.split('key=').pop()
+  else if (val.indexOf('split') != -1) splitLevel = val.split('split=').pop()
+  else if (val.indexOf('exclude') != -1)
+
+    if (val.indexOf('exclude=') != -1) {
+      excludeTags = val.split('exclude=').pop()
+      console.log()
+      console.log('excludeTags')
+      console.log(excludeTags)
+      console.log(excludeTags.length)
+
+      if (excludeTags.length == 0) throw 'The exclude parameter was provided without value. Ain\'t nobody got time for that. Put something there, boy!'
+      else if (excludeTags.indexOf(',') == -1) throw 'The parameter provided doesn\'t seem to be a tag range. Do you even lift? Please, check the content provided.'
+      else if (
+        console.log(
+
+          (
+            () => {
+              return (
+                excludeTags
+                  .split(',')
+                  .map(
+                  (e) => { return isNaN(e) }
+                  ) == 0
+              )
+            }
+          )()
+        )
+      )
+        throw 'You have not provided numbers inbetween commas. Fuck you, you know. Like, what the fuck? Aren\' you the fucking developer of this shit? I don\'t even know what to tell you, dude. Like, put on some fucking reasonable parameter values and THEN we\'ll talk, ok? JEEZ...'
+    }
+
+});
+
+var pass =
+  (
+    indexPath.length != 0
+    &&
+    splitLevel.length != 0
+    &&
+    searchKey.length != 0
+  )
+
+if (!pass) throw 'Please, provide a parameter for "folder", "key" and "split"'
+
+
+
+
+require('./src/app.js').execute(indexPath, splitLevel, searchKey, notWrite, logFormat, excludeTags)
---
+++ src/app.js
@@ -0,0 +1,268 @@
+module.exports = {
+    execute: (indexPath, splitLevel, searchKey, notWrite, logFormat, excludeTags) => {
+        'use strict'
+
+        var
+            fs = require('fs')
+            , writeContainerFiles = (containerName, allGTMs, ranges, container) => {
+                containerName = containerName ? containerName : '(NOT SET)'
+
+                var
+                    range = ''
+                    , fullContainerName = []
+                    , newFullContainer = []
+                    ;
+
+                allGTMs.forEach((e, i, a) => {
+                    fullContainerName = []
+
+                    range = ranges[i].tags[0] + '_' + ranges[i].tags[1]
+
+                    fullContainerName.push(i + 1)
+                    fullContainerName.push(containerName)
+                    fullContainerName.push(ranges[i].level)
+                    fullContainerName.push(range)
+                    fullContainerName = fullContainerName.join(' - ')
+                    fullContainerName += '.json'
+
+                    newFullContainer = JSON.parse(JSON.stringify(container))
+                    newFullContainer.containerVersion.tag = e
+
+                    console.log('\nSaving file:', fullContainerName)
+                    console.log(newFullContainer.containerVersion.tag.length, 'tags')
+                    console.log(newFullContainer.containerVersion.variable.length, 'variables')
+                    console.log(newFullContainer.containerVersion.trigger.length, 'triggers')
+
+                    fs.writeFile('Processed containers\\' + fullContainerName
+                        , JSON.stringify(newFullContainer), (err) => {
+                            if (err) throw err;
+
+                            console.log('\nFile saved')
+                        });
+                });
+            }
+            , writeLogs = (containerName, allGTMs, ranges, container) => {
+                containerName = containerName ? containerName : '(NOT SET)'
+
+                var
+                    range = ''
+                    , fullContainerName = []
+                    , newFullContainer = []
+                    ;
+
+                var
+                    logList = []
+                    , logSplit = []
+                    ;
+
+                allGTMs.forEach((e, i, a) => {
+                    fullContainerName = []
+
+                    range = ranges[i].tags[0] + '_' + ranges[i].tags[1]
+
+                    fullContainerName.push(i + 1)
+                    fullContainerName.push(containerName)
+                    fullContainerName.push(ranges[i].level)
+                    fullContainerName.push(range)
+                    fullContainerName = fullContainerName.join(' - ')
+                    fullContainerName += '.json'
+
+                    newFullContainer = JSON.parse(JSON.stringify(container))
+                    newFullContainer.containerVersion.tag = e
+
+                    console.log('\nSaving file:', fullContainerName)
+                    console.log(newFullContainer.containerVersion.tag.length, 'tags')
+                    console.log(newFullContainer.containerVersion.variable.length, 'variables')
+                    console.log(newFullContainer.containerVersion.trigger.length, 'triggers')
+
+                    logList.push('----- ' + fullContainerName + '\n\n' + e.map((x) => { return x.name }).join('\n'))
+                    logSplit.push(fullContainerName + ',' + e.map((x) => { return x.name }).join(','))
+                }
+                );
+
+                fs.writeFileSync(
+                    'tag-name-log.txt'
+                    , logList.join('\n\n'
+                    )
+                    , 'utf-8'
+                )
+
+                fs.writeFileSync(
+                    'tag-name-log-splitable.txt'
+                    , logSplit.join(';'
+                    )
+                    , 'utf-8'
+                )
+            }
+            ;
+
+        var
+            paths =
+                (() => {
+                    return {
+                        root: indexPath.join('\\')
+                        , processed: indexPath.join('\\') + '\\\Processed container'
+                        , original: indexPath.join('\\') + '\\\Original containers'
+                    }
+                })()
+            , folderFileArray = fs.readdirSync(paths.original)
+            , folderFile = folderFileArray.filter((e, i, a) => {
+
+                var
+                    arr = searchKey.split(' ')
+                    , passCount = 0
+                    ;
+
+                arr.forEach((ee) => {
+                    if (e.indexOf(arr[i]) != -1) {
+                        passCount++
+                    }
+                })
+
+                return passCount == arr.length
+            })
+            ;
+
+        if (folderFile.length != 1)
+            throw 'Couldn\'t get an unique match with the search key provided. Could you check them and try again, please?\nHere are the folder files to help you up!\n' + folderFileArray.map((e, i, a) => {
+                return '\n' + i + '| ' + e
+            })
+        else folderFile = folderFile[0]
+
+        console.log('- File chosen:', folderFile)
+
+        var container = JSON.parse(fs.readFileSync([paths.original, folderFile].join('\\')));
+
+        console.log('-', container.containerVersion.tag.length + ' tags')
+        console.log('-', container.containerVersion.variable.length + ' variable')
+        console.log('-', container.containerVersion.trigger.length + ' trigger')
+
+        // Separando a tag de webdev
+        var customCheckout = container.containerVersion.tag.filter((e, i, a) => {
+            return (e.name == 'Script - Checkout Custom')
+        })[0];
+
+        var
+            allGTMs = []
+            , formatArrToObj = (ranges) => {
+                var
+                    result = []
+                    , levelHash = {
+                        '1': 'Full'
+                        , '2': 'Half'
+                        , '4': 'Quarter'
+                        , '8': 'Eigth'
+                        , '16': 'Sixteenth'
+                        , '32': '32th'
+                    }
+
+                ranges.forEach((e, i, a) => {
+                    e.forEach((ee, ii, aa) => {
+
+                        result.push(
+                            {
+                                level: levelHash[e.length + '']
+                                , tags: ee
+                            }
+                        )
+
+                    });
+
+                });
+
+                return result;
+            }
+            , listTagsByName = (fileName) => {
+
+                console.log('-- listTagsByName called')
+
+                console.log(
+                    JSON.parse(
+                        fs.readFileSync(
+                            paths.processed + '\\' + fileName
+                            , 'utf-8')
+                    )
+                        .containerVersion
+                        .tag
+                        .map((e) => { return e.name })
+                )
+
+            }
+            , newTagsArrays = (ranges) => {
+                var containers = [];
+
+                ranges.forEach((e, i, a) => {
+
+                    var croppedGTM = container.containerVersion.tag.filter((ee, ii, aa) => {
+
+                        var pass =
+                            (
+                                ii >= e.tags[0]
+                                &&
+                                ii <= e.tags[1]
+                                &&
+                                ee.name != 'Script - Checkout Custom'
+                            )
+
+                        return pass
+                    });
+
+                    containers.push(croppedGTM)
+
+                    containers[i].push(customCheckout);
+
+                });
+                return containers;
+            }
+            , createRanges = (max, levels) => {
+
+                let allRangesByLevel = []
+
+                for (let i = 0; i <= levels; i++) {
+                    let
+                        levelRanges = []
+                        , pot = Math.pow(2, i)
+                        , step = Math.ceil(max / pot)
+
+                    for (let j = 1; j <= pot; j++) {
+                        let
+                            range = []
+                            , top = Math.ceil(j * step) > max ? max : Math.ceil(j * step)
+
+                        levelRanges.push(
+                            levelRanges.length == 0 ? [0, top] : [previousTop + 1, top]
+                        )
+
+                        var previousTop = Math.ceil(j * step) > max ? max : Math.ceil(j * step)
+
+                    }
+
+
+                    allRangesByLevel.push(levelRanges)
+                }
+                return allRangesByLevel
+            }
+            , ranges = formatArrToObj(createRanges(container.containerVersion.tag.length, splitLevel))
+            , finalize = (text) => {
+                console.log();
+                console.log(text);
+                console.log();
+            }
+            , execute = (fileBaseName) => {
+                if (notWrite)
+                    writeContainerFiles(fileBaseName, newTagsArrays(ranges), ranges, container)
+
+                writeLogs(fileBaseName, newTagsArrays(ranges), ranges, container, logFormat)
+
+                finalize('----- That\'s all, folks! -----')
+            }
+            ;
+
+        if (excludeTags.length > -1) {
+            if (excludeTags.indexOf(',') != -1)
+                if (excludeTags.indexOf(',') != -1)
+                    throw 'Pelse'
+        } else execute('CC - Importable');
+
+    }
+}
